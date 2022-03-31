9b2ed71a5f469a4bb6f4170e9e5f70496822d348
DuncanHuang <duncanhuang@gaiusauto.com>
2022-03-31 15:00:15 +0800

    chore: remove useless code

--- generate.sh
+++ generate.sh
@@ -1,6 +1,3 @@
-# protoc --proto_path=./proto --go_out==import_path=${pkg},plugins=grpc:./service --go_opt=paths=source_relative proto/*/*.proto 
-# protoc --proto_path=./proto --go_out=./service --go_opt=paths=source_relative proto/$(basename "$dir")/*/*.proto
-# protoc --proto_path=./proto --go-grpc_out=. --go-grpc_opt=paths=source_relative proto/*/*.proto 
 #!/bin/bash
 
 export PATH="$PATH:$(go env GOPATH)/bin"
